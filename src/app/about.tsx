ac9599e1a3bd27a139297c6a76d904af13640a58
evanbacon <bacon@expo.io>
2023-12-13 11:45:46 -0600

    add native support

--- src/app/about.tsx
+++ src/app/about.tsx
@@ -1,22 +1,11 @@
-import { CalendarDateRangePicker } from "@/components/date-range-picker";
-import { Button } from "@/components/ui/button";
 import Head from "expo-router/head";
 
-export default function DashboardPage() {
+export default function Route() {
   return (
     <>
       <Head>
         <title>About</title>
       </Head>
-      {/* <div className="flex-1 space-y-4 p-8 pt-6">
-        <div className="flex items-center justify-between space-y-2">
-          <h2 className="text-3xl font-bold tracking-tight">About</h2>
-          <div className="flex items-center space-x-2">
-            <CalendarDateRangePicker />
-            <Button>Download</Button>
-          </div>
-        </div>
-      </div> */}
     </>
   );
 }
--- src/app/contact.tsx
+++ src/app/contact.tsx
@@ -1,18 +1,11 @@
-import { CalendarDateRangePicker } from "@/components/date-range-picker";
-import { Button } from "@/components/ui/button";
 import Head from "expo-router/head";
 
-export default function DashboardPage() {
+export default function Route() {
   return (
     <>
       <Head>
-        <title>Contact</title>
+        <title>Contacts</title>
       </Head>
-      {/* <div className="flex-1 space-y-4 p-8 pt-6">
-        <div className="flex items-center justify-between space-y-2">
-          <h2 className="text-3xl font-bold tracking-tight">Contact</h2>
-        </div>
-      </div> */}
     </>
   );
 }
